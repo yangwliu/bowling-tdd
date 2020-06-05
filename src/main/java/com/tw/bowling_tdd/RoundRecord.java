48e1819b00a06030b0a649f05bebbbca8897739a
yangwliu <yangwliu@thoughtworks.com>
2020-06-05 23:58:04 +0800

    [Liu Yang] #NA Add validator for roundNumber of RoundRecord.

--- src/main/java/com/tw/bowling_tdd/RoundRecord.java
+++ src/main/java/com/tw/bowling_tdd/RoundRecord.java
@@ -1,5 +1,6 @@
 package com.tw.bowling_tdd;
 
+import com.tw.bowling_tdd.exception.InValidRoundNumberException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,16 +11,24 @@ public class RoundRecord {
   private List<Integer> knockedBalls;
 
   public RoundRecord(int roundNumber, RoundResultEnum result, List<Integer> knockedBalls) {
+    validateRoundNumber(roundNumber);
     this.roundNumber = roundNumber;
     this.result = result;
     this.knockedBalls = knockedBalls;
   }
 
   public static RoundRecord buildSpikeRoundRecord(int roundNumber) {
+    validateRoundNumber(roundNumber);
     if (roundNumber < 10) {
       return new RoundRecord(roundNumber, RoundResultEnum.STRIKE, Arrays.asList(10, 0));
     }
     return new RoundRecord(roundNumber, RoundResultEnum.STRIKE, Arrays.asList(10, 10, 10));
+  }
+
+  static void validateRoundNumber(int roundNumber) {
+    if (roundNumber < 1 || roundNumber > 10) {
+      throw new InValidRoundNumberException();
+    }
   }
 
   public int getRoundNumber() {
---
+++ src/main/java/com/tw/bowling_tdd/exception/InValidRoundNumberException.java
@@ -0,0 +1,8 @@
+package com.tw.bowling_tdd.exception;
+
+public class InValidRoundNumberException extends RuntimeException {
+
+  public InValidRoundNumberException() {
+    super();
+  }
+}
